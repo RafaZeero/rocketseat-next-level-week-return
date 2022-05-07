9fc09f2e3ef1b24085e238e46b3ba0b4109114b4
rafazeero <rafaa99@gmail.com>
2022-05-07 15:03:29 -0300

    Reorganizing folders

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import cors from 'cors'
+import express from 'express'
+import { routes } from './routes'
+
+const app = express()
+const PORT = 3333
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(PORT, () =>
+  console.log(`HTTP server running at http://localhost:${PORT}`)
+)
